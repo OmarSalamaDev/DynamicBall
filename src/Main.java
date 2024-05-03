1135d516b378e6ae8791092ead24cb1c0eb17379
OmarSalamaDev <138377317+OmarSalamaDev@users.noreply.github.com>
2024-05-03 14:24:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,75 +1,146 @@
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 
 public class Main extends Application {
 
 
-    DynamicBall ball = new DynamicBall();
-    DynamicBall secondaryBall = new DynamicBall();
+/* >>>> global variables and objects  <<<< */
+
+
+    // objects
+    public DynamicBall A = new DynamicBall();
+    public double rA = 30;
+    public double mA = 1;
+    public DynamicBall B = new DynamicBall();
+    public double rB = 30;
+    public double mB = 1;
+
+    // collision velocity
+    public double vxA;
+    public double vxA_;
+    public double vyA;
+    public double vyA_;
+    public double vxB;
+    public double vxB_;
+    public double vyB;
+    public double vyB_;
+
+    // mouse pointer position
+    public double initialX;
+    public double initialY;
+    public double finalX;
+    public double finalY;
+
+    // motion animation timer
+    public AnimationTimer timer;
 
 
     @Override
     public void start(Stage primaryStage) {
+
+        // setup
         Pane root = new Pane();
-        Scene primaryScene = new Scene(root, 800, 600, Color.BLACK);
+        Scene primaryScene = new Scene(root, 800, 400, Color.BLACK);
         primaryStage.setScene(primaryScene);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Bouncing Ball");
         primaryStage.show();
 
-    // primary ball setup
-        ball.setRadius(20);
-        ball.setFill(Color.WHITE);
-        ball.setCenterX(50);
-        ball.setCenterY(primaryScene.getHeight() - ball.getRadius());
-        ball.setSceneBorders(0, primaryScene.getHeight(), primaryScene.getWidth(), 0);
-        root.getChildren().add(ball);
+        // object A <Dynamic ball>
+        A.setRadius(rA);
+        A.setFill(Color.WHITE);
+        A.setCenterX(50);
+        A.setCenterY(primaryScene.getHeight() - A.getRadius());
+        A.setSceneBorders(0, primaryScene.getHeight(), primaryScene.getWidth(), 0);
+        root.getChildren().add(A);
+        enableMotion(A);
 
-    // secondary ball setup
-        /*
-        secondaryBall.setRadius(20);
-        secondaryBall.setFill(Color.RED);
-        secondaryBall.setCenterX(primaryScene.getWidth() /3);
-        secondaryBall.setCenterY(primaryScene.getHeight() - primaryBall.getRadius());
-        secondaryBall.setSceneBorders(0, primaryScene.getHeight(), primaryScene.getWidth(), 0);
-        secondaryBall.enableMotion();
-        root.getChildren().add(secondaryBall);*/
+        //object B <Dynamic ball>
+        B.setRadius(rB);
+        B.setFill(Color.RED);
+        B.setCenterX(300);
+        B.setCenterY(primaryScene.getHeight() - B.getRadius());
+        B.setSceneBorders(0, primaryScene.getHeight(), primaryScene.getWidth(), 0);
+        root.getChildren().add(B);
+        enableMotion(B);
 
+        // collision handling
+        AnimationTimer collision = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                if (Math.sqrt(Math.pow((A.getCenterX() - B.getCenterX()), 2) + Math.pow((A.getCenterY() - B.getCenterY()), 2)) <= Math.abs(A.getRadius() + B.getRadius())) {
 
-        //primaryBall.getBoundsInParent().intersects(primaryBall.getBoundsInParent());
+                    // velocity just before collision
+                    vxA = A.getVx();
+                    vyA = A.getVy();
+                    vxB = B.getVx();
+                    vyB = B.getVy();
 
+                    // velocity just after collision
+                    vxA_ = (mA-mB)*vxA/(mA+mB)+2*mB*vxB/(mA+mB);
+                    vyA_ = (mA-mB)*vyA/(mA+mB)+2*mB*vyB/(mA+mB);
+                    vxB_ = 2*mA*vxA/(mA+mB)-(mA-mB)*vxB/(mA+mB);
+                    vyB_ = 2*mA*vyA/(mA+mB)-(mA-mB)*vyB/(mA+mB);
 
+                    // check for overlying
+                    if (Math.abs(Math.sqrt(Math.pow((A.getCenterX() - B.getCenterX()), 2) + Math.pow((A.getCenterY() - B.getCenterY()), 2)) - (A.getRadius() + B.getRadius())) > 0) {
 
+                        // set separation to prevent overlapping
+                        double dx = (B.getCenterX() - A.getCenterX()) * 0.02;
+                        double dy = (B.getCenterY() - A.getCenterY()) * 0.02;
+
+                        // add separation
+                        A.setCenterX(A.getCenterX() - dx);
+                        A.setCenterY(A.getCenterY() - dy);
+                        B.setCenterX(B.getCenterX() + dx);
+                        B.setCenterY(B.getCenterY() + dy);
+                    }
+
+                    // apply the new velocity
+                    A.setVx(vxA_);
+                    A.setVy(vyA_);
+                    B.setVx(vxB_);
+                    B.setVy(vyB_);
+                }
+            }
+        };
+        collision.start();
+
+        // mouse events
         primaryScene.setOnMousePressed(e -> {
-            ball.setInitialMousePosition(e.getSceneX(), e.getSceneY());
+            initialX = e.getSceneX();
+            initialY = e.getSceneY();
         });
         primaryScene.setOnMouseReleased(e -> {
-            ball.setFinalMousePosition(e.getSceneX(), e.getSceneY());
+            finalX = e.getSceneX();
+            finalY = e.getSceneY();
+            A.setVelocity(initialX, initialY, finalX, finalY);
         });
-
-        primaryScene.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.SPACE){
-                ball.setCenterY(200);
-                ball.enableMotion();
-            }
-        });
-
-        /*primaryScene.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.RIGHT) {
-                ball.setVx(50);
-                ball.enableMotion();
-            }
-        });*/
-
-
-
-
-
     }
+
+
+/* >>>> enable motion for the object  <<<< */
+
+
+    public void enableMotion(DynamicBall ball) {
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                ball.updatePosition();
+            }
+        };
+        timer.start();
+    }
+
 
     public static void main(String[] args) {
         launch(args);
